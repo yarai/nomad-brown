bc613e554e32d69ec9b1062b2f3cee26efb55fe7
Yuta Arai <yuta_arai@brown.edu>
2016-06-19 07:07:10 -0700

    more fonts

--- static/js/yelp.js
+++ static/js/yelp.js
@@ -1,2 +1,9 @@
 function Yelp() {
+	this.key = 'lnnQgMHhqPwJ_ofQ3Td-Hw';
+	this.secret = '-8t4OJdYNuBuDMPVRGeZQa2B3og';
+	this.token = 'LhUxV_6ECjmJhMDLW2Qadk-ZBT3nhjOe';
+	this.tokenSecret = 'ynX4K_ikxxjp-LUorCsPgzKZflU';
+	this.request = function() {
+		
+	}
 }
