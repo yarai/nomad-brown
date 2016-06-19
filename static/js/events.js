97fcb24bc4eafbe8f18ff7754da877512ec91fcf
Yuta Arai <yuta96b@gmail.com>
2016-06-19 09:42:59 -0700

    yay

--- static/js/events.js
+++ static/js/events.js
@@ -1,1 +1,8 @@
-console.log("hello");
+
+var location = getParameterByName("location")
+var end_time = getParameterByName("endtime")
+var travel = getParameterByName("travel")
+
+var eb = Eventbrite()
+eb.request(location, end_time)
+console.log("GOT HERE")
