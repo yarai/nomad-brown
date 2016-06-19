e81716cc76948eb9aceb74f05a6bb8937b4c50f0
Yuta Arai <yuta_arai@brown.edu>
2016-06-19 05:48:42 -0700

    setup project

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template, redirect, url_for
+import dataset
+
+app = Flask(__name__)
+
+# Connnect to database
+db = dataset.connect('sqlite:///file.db')
+
+# when someone sends a GET to / render sign_form.html
+@app.route('/', methods=['GET'])
+def sign_form():
+    return render_template('pages/index.html')
+
+app.run()
