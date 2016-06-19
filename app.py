058a2783dd24837a25a73a62a71208cc076a8460
Yuta Arai <yuta_arai@brown.edu>
2016-06-19 06:34:19 -0700

    changed requirements

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for
-import dataset
 
 app = Flask(__name__)
-
-# Connnect to database
-db = dataset.connect('sqlite:///file.db')
 
 # when someone sends a GET to / render sign_form.html
 @app.route('/', methods=['GET'])
